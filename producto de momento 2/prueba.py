76eacb79bc459bac8bacf939f8d55041c6ab7d7c
jhon97michael <112593253+jhon97michael@users.noreply.github.com>
2023-11-22 10:14:30 -0500

    Se realiza el aplicativo del momento 2

--- producto de momento 2/prueba.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-
-conexion = mysql.connector.connect(user='root', password='root',
-                                    host='localhost',
-                                    database='divina_providencia',
-                                    port='3306')
-print(conexion)
